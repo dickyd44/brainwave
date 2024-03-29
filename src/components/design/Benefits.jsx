d8002a9011e340a64832570fc4b345fc33ef32d5
dikydrmwn08@gmail.com <dikydrmwn08@gmail.com>
2024-03-30 01:31:21 +0800

    setup design benefits & collaboration

---
+++ src/components/design/Benefits.jsx
@@ -0,0 +1,5 @@
+export const GradientLight = () => {
+  return (
+    <div className="absolute top-8 left left-1/4 w-full aspect-square bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-70% pointer-events-none" />
+  );
+};
